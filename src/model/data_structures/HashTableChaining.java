636e7980156e2460d4440d4ae2a1ce1256f6b54b
jsgonzalez15b <47197962+jsgonzalez15b@users.noreply.github.com>
2019-03-19 21:17:28 -0500

    Update HashTableChaining.java
    updated (falta actualizar el metodo hash y revisar el error en iterador)

--- src/model/data_structures/HashTableChaining.java
+++ src/model/data_structures/HashTableChaining.java
@@ -3,53 +3,183 @@ package model.data_structures;
 public class HashTableChaining <K extends Comparable<K>,V> implements IHashTableChaining<K,V>
 {
 	//Atributos
-		/**
-		 * Arreglo de tamaño variable que almacena la informacion del Hash (NO ARREGLO DINAMICO)
-		*/
-		
-		/**
-		 * Numero de llaves en uso
-		*/
-		/**
-		 * Numero de asociados
-		*/
-		//Constructor
-		public HashTableChaining()
-		{
-			
-		}
-			
-		//Métodos
+	/**
+	 * Arreglo que almacena la informacion del Hash implementado para variar tamaÃ±o (NO ARREGLO DINAMICO)
+	 */
+	private Dupla[] hashTable;
 
-		
-		/**
-		 * Agregar una dupla (K,V) a la tabla. Si la llave K existe reemplaza su valor V asociado
-		 * V no puede ser null.
-		*/
-		public void	put(K pLlave, V pValor)
+	/**
+	 * Numero de indices en uso
+	 */
+	private int nCarga;
+
+	/**
+	 * Factor de carga maximo
+	 */
+	private double factorCarga;
+
+	//Constructor
+	public HashTableChaining()
+	{
+		hashTable= new Dupla[10];
+		factorCarga=0.5;
+	}
+
+	//MÃ©todos
+	/**
+	 * Calcula el hash asociado a la llave
+	 * @return.
+	 */
+	public int	hash(K pLlave)
+	{
+		return 1; //CAMBIAR a una funcion dependiente del tamano
+	}
+
+	/**
+	 * Actualiza la posicion de todos los elementos del hashTable segun su llave y el nuevo tamano
+	 */
+	public void	reHash()
+	{
+		//CAMBIAR
+		Dupla[] copiaHash= hashTable; //se crea un copia con las duplas actuales
+		hashTable = new Dupla[hashTable.length*2];//se aumenta la tabla del HashTableLinear
+
+		for ( int i = 0; i < copiaHash.length; i++)//se obtienen los nuevos indices 
 		{
-			
-		}
-		/**
-		 * Obtener el valor V asociada a la llave K. V no puede ser null.
-		*/
-		public V	get(K pLlave)
+			if(copiaHash[i]!=null)
+			{
+				put((K)copiaHash[i].getKey(),(V)copiaHash[i].getValue());
+			}
+		} 
+	}
+
+	/**
+	 * Agregar una dupla (K,V) a la tabla. Si la llave K existe reemplaza su valor V asociado
+	 * V no puede ser null.
+	 */
+	public void	put(K pLlave, V pValor)
+	{
+		int pIndice= hash(pLlave);//calculo indice de la dupla segun tamano actual
+
+		//booleanos para indicar el estado de insercion de la dupla para linear colision
+		boolean insertado = false;
+		boolean colision = false;
+
+		while(!insertado)//se recorre la tabla hasta insertar el elemento
 		{
-			return null;
+			if(hashTable[pIndice]==null)//cuando se encuente una dupla vacia esta es inicializada con la nueva dupla
+			{
+				hashTable[pIndice]= new Dupla(pLlave,pValor);
+				insertado=true; //se actualiza estado de insercion
+			}
+			else
+			{
+				colision = true; //ya que la dupla esta ocupada ocurrio una colision
+				hashTable[pIndice].add(pLlave,pValor);//se agrega a la dupla en esta posicion
+				insertado=true; //se actualiza estado de insercion
+			}
 		}
-		/**
-		 * Borrar la dupla asociada a la llave K. Se obtiene el valor V asociado a la llave K.
-		 * Se obtiene null si la llave K no existe.
-		*/
-		public V	delete(K pLlave)
+
+		if(!colision) //si no hubo colision se actualiza el numero de carga en uso
 		{
-			return null;
+			nCarga++;
+			if(nCarga/hashTable.length>=factorCarga)
+			{
+				reHash();
+			}
 		}
-		/**
-		 * Conjunto de llaves K presentes en la tabla.
-		*/
-		public Iterador<K>	keys()
+	}
+	/**
+	 * Obtener el valor V asociada a la llave K. V no puede ser null.
+	 */
+	public V get(K pLlave)
+	{
+		int indice = hash(pLlave);
+		V elValueBuscado = null;
+		if(hashTable[indice]!=null) //necesariamente esta condicion debe cumplirse
 		{
-			return null;
+			for(int i=0;i<hashTable[indice].chain.darTamano();i++)//busqueda en arreglo de duplas
+			{
+				if(hashTable[indice].chain.darElemento(i)!=null)
+				{
+					if(hashTable[indice].chain.darElemento(i).getKey().compareTo(pLlave)==0)
+					{
+						elValueBuscado = (V) hashTable[indice].chain.darElemento(i).getValue();
+					}
+				}
+			}
+			elValueBuscado = (V) hashTable[indice].getValue();
 		}
+		return elValueBuscado;
+	}
+	/**
+	 * Borrar la dupla asociada a la llave K. Se obtiene el valor V asociado a la llave K.
+	 * Se obtiene null si la llave K no existe.
+	 */
+	public V	delete(K pLlave)
+	{
+		int indice = hash(pLlave);
+		V elValueBuscado = null;
+		if(hashTable[indice]!=null) //necesariamente esta condicion debe cumplirse
+		{
+			for(int i=0;i<hashTable[indice].chain.darTamano();i++)//busqueda en arreglo de duplas
+			{
+				if(hashTable[indice].chain.darElemento(i)!=null)
+				{
+					if(hashTable[indice].chain.darElemento(i).getKey().compareTo(pLlave)==0)
+					{
+						elValueBuscado = (V) hashTable[indice].chain.darElemento(i).getValue();
+						hashTable[indice].chain.eliminar(hashTable[indice].chain.darElemento(i)); //elimina la duplaBuscada
+					}
+				}
+			}
+		}
+		return elValueBuscado;
+	}
+	/**
+	 * Conjunto de llaves K presentes en la tabla.
+	 */
+	public Iterador<K>	keys()
+	{
+		boolean encontrada= false;
+		boolean primeraEncontrada=false;
+		int i = 0;
+		Nodo<K> firstKey = null;
+		Nodo<K> keyActual = null;
+		Iterador<Nodo<K>> llavero = null;
+
+		while(!encontrada && i<hashTable.length)//busqueda de primera y segunda dupla en HashTableLinear
+		{
+			if(hashTable[i]==null)
+			{
+				i++;
+			}
+			else if(!primeraEncontrada)
+			{
+				firstKey = new Nodo( hashTable[i].getKey()); //creacion del primer nodo key
+				primeraEncontrada=true;
+			}
+			else
+			{
+				keyActual = new Nodo( hashTable[i].getKey()); //creacion del segundo nodo key
+				firstKey.setSiguiente(keyActual);
+				encontrada=true;
+			}
+		}
+		if(encontrada) 
+		{
+			for(int j=0;j<hashTable.length;j++)//creacion de una lista de nodos simplemente encadenada
+			{
+				if(hashTable[i]!=null)
+				{
+					keyActual.setSiguiente(new Nodo(hashTable[i].getKey()));
+					keyActual=keyActual.darSiguiente();
+				}
+			}
+		}
+
+		llavero = new Iterador<firstKey>;
+		return llavero;
+	}
+
 }
