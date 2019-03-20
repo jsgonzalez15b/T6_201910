5d3f1b3e6ae36de85f3af5e072bcbbe5c555fde8
jsgonzalez15b <47197962+jsgonzalez15b@users.noreply.github.com>
2019-03-19 21:15:24 -0500

    Update HashTableLinear.java
    updated XD

--- src/model/data_structures/HashTableLinear.java
+++ src/model/data_structures/HashTableLinear.java
@@ -3,106 +3,167 @@ package model.data_structures;
 public class HashTableLinear<K extends Comparable<K>,V> implements IHashTableLinear<K,V>
 {
 	//Atributos
-		/**
-		 * Arreglo que almacena la informacion del Hash implementado para variar tamaño (NO ARREGLO DINAMICO)
-		*/
-		private Dupla[] hashTable;
-		
-		/**
-		 * Numero de indices en uso
-		*/
-		private int nCarga;
-		
-		/**
-		 * Factor de carga maximo
-		*/
-		private double factorCarga;
-		
-		//Constructor
-		public HashTableLinear()
+	/**
+	 * Arreglo que almacena la informacion del Hash implementado para variar tamaÃ±o (NO ARREGLO DINAMICO)
+	 */
+	private Dupla[] hashTable;
+
+	/**
+	 * Numero de indices en uso
+	 */
+	private int nCarga;
+
+	/**
+	 * Factor de carga maximo
+	 */
+	private double factorCarga;
+
+	//Constructor
+	public HashTableLinear()
+	{
+		hashTable= new Dupla[10];
+		factorCarga=0.75;
+	}
+	//MÃ©todos
+
+	/**
+	 * Calcula el hash asociado a la llave
+	 * @return.
+	 */
+	public int	hash(K pLlave)
+	{
+		return 1; //CAMBIAR a una funcion dependiente del tamano
+	}
+
+	/**
+	 * Actualiza la posicion de todos los elementos del hashTable segun su llave y el nuevo tamano
+	 */
+	public void	reHash()
+	{
+		//CAMBIAR
+		Dupla[] copiaHash= hashTable; //se crea un copia con las duplas actuales
+		hashTable = new Dupla[hashTable.length*2];//se aumenta la tabla del HashTableLinear
+
+		for ( int i = 0; i < copiaHash.length; i++)//se obtienen los nuevos indices 
 		{
-			hashTable= new Dupla[10];
-			factorCarga=0.75;
-		}
-		//Métodos
-		
-		/**
-		 * Calcula el hash asociado a la llave
-		 * @return.
-		*/
-		public int	hash(K pLlave)
-		{
-			return 1; //CAMBIAR
-		}
-		
-		/**
-		 * Actualiza la posicion de todos los elementos del hashTable segun su llave y el nuevo tamano
-		*/
-		public void	rehash(K pLlave)
-		{
-			//CAMBIAR
-		}
-		
-		/**
-		 * Agregar una dupla (K,V) a la tabla. Si la llave K existe reemplaza su valor V asociado
-		 * V no puede ser null.
-		*/
-		public void	put(K pLlave, V pValor)
-		{
-			int pIndice= hash(pLlave);//calculo indice de la dupla
-			
-			//booleanos para indicar el estado de insercion de la dupla para linear colision
-			boolean insertado = false;
-			boolean colision = false;
-			
-			while(!insertado)//se recorre la tabla hasta insertar el elemento
+			if(copiaHash[i]!=null)
 			{
-				if(hashTable[pIndice]==null)//cuando se encuente una dupla vacia esta es inicializada con la nueva dupla
+				put((K)copiaHash[i].getKey(),(V)copiaHash[i].getValue());
+			}
+		} 
+	}
+
+	/**
+	 * Agregar una dupla (K,V) a la tabla. Si la llave K existe reemplaza su valor V asociado
+	 * V no puede ser null.
+	 */
+	public void	put(K pLlave, V pValor)
+	{
+		int pIndice= hash(pLlave);//calculo indice de la dupla segun tamano actual
+
+		//booleanos para indicar el estado de insercion de la dupla para linear colision
+		boolean insertado = false;
+		boolean colision = false;
+
+		while(!insertado)//se recorre la tabla hasta insertar el elemento
+		{
+			if(hashTable[pIndice]==null)//cuando se encuente una dupla vacia esta es inicializada con la nueva dupla
+			{
+				hashTable[pIndice]= new Dupla(pLlave,pValor);
+				insertado=true; //se actualiza estado de insercion
+			}
+			else
+			{
+				colision = true; //ya que la dupla esta ocupada ocurrio una colision
+				pIndice++; //se busca en el siguiente bloque en la siguiente iteracion
+				if(pIndice==hashTable.length) //cuando se llega al fin del arreglo se reanuda la busqueda desde cero
 				{
-					hashTable[pIndice]= new Dupla(pLlave,pValor);
-					insertado=true; //se actualiza estado de insercion
-				}
-				else
-				{
-					colision = true; //ya que la dupla esta ocupada ocurrio una colision
-					pIndice++; //se busca en el siguiente bloque en la siguiente iteracion
-					if(pIndice==hashTable.length) //cuando se llega al fin del arreglo se reanuda la busqueda desde cero
-					{
-						pIndice=0; 
-					}
+					pIndice=0; 
 				}
 			}
-			
-			if(!colision) //si no hubo colision se actualiza el numero de carga en uso
+		}
+
+		if(!colision) //si no hubo colision se actualiza el numero de carga en uso
+		{
+			nCarga++;
+			if(nCarga/hashTable.length>=factorCarga)
 			{
-				nCarga++;
-				if(nCarga/hashTable.length>=factorCarga)
+				reHash();
+			}
+		}
+	}
+	/**
+	 * Obtener el valor V asociada a la llave K. V no puede ser null.
+	 */
+	public V	get(K pLlave)
+	{
+		int indice = hash(pLlave);
+		V elValueBuscado = null;
+		if(hashTable[indice]!=null) //necesariamente esta condicion debe cumplirse
+		{
+			elValueBuscado = (V) hashTable[indice].getValue();
+		}
+		return elValueBuscado;
+	}
+	/**
+	 * Borrar la dupla asociada a la llave K. Se obtiene el valor V asociado a la llave K.
+	 * Se obtiene null si la llave K no existe.
+	 */
+	public V	delete(K pLlave)
+	{
+		int indice = hash(pLlave);
+		V elValueBuscado = null;
+		if(hashTable[indice]!=null) //necesariamente esta condicion debe cumplirse
+		{
+			elValueBuscado = (V) hashTable[indice].getValue();
+			hashTable[indice]= null;
+		}
+		return elValueBuscado;
+	}
+	/**
+	 * Conjunto de llaves K presentes en la tabla.
+	 */
+	public Iterador<K>	keys()
+	{
+		boolean encontrada= false;
+		boolean primeraEncontrada=false;
+		int i = 0;
+		Nodo<K> firstKey = null;
+		Nodo<K> keyActual = null;
+		Iterador<Nodo<K>> llavero = null;
+
+		while(!encontrada && i<hashTable.length)//busqueda de primera y segunda dupla en HashTableLinear
+		{
+			if(hashTable[i]==null)
+			{
+				i++;
+			}
+			else if(!primeraEncontrada)
+			{
+				firstKey = new Nodo( hashTable[i].getKey()); //creacion del primer nodo key
+				primeraEncontrada=true;
+			}
+			else
+			{
+				keyActual = new Nodo( hashTable[i].getKey()); //creacion del segundo nodo key
+				firstKey.setSiguiente(keyActual);
+				encontrada=true;
+			}
+		}
+		if(encontrada) 
+		{
+			for(int j=0;j<hashTable.length;j++)//creacion de una lista de nodos simplemente encadenada
+			{
+				if(hashTable[i]!=null)
 				{
-					reHash();
+					keyActual.setSiguiente(new Nodo(hashTable[i].getKey()));
+					keyActual=keyActual.darSiguiente();
 				}
 			}
 		}
-		/**
-		 * Obtener el valor V asociada a la llave K. V no puede ser null.
-		*/
-		public V	get(K pLlave)
-		{
-			return null;
-		}
-		/**
-		 * Borrar la dupla asociada a la llave K. Se obtiene el valor V asociado a la llave K.
-		 * Se obtiene null si la llave K no existe.
-		*/
-		public V	delete(K pLlave)
-		{
-			return null;
-		}
-		/**
-		 * Conjunto de llaves K presentes en la tabla.
-		*/
-		public Iterador<K>	keys()
-		{
-			return null;
-		}
+
+		llavero = new Iterador<firstKey>;
+		return llavero;
+	}
 
 }
